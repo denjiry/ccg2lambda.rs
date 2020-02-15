dbee8b404e34e4bd80e16f5610700d05981d3942
kento tajiri <k.denjiry@gmail.com>
2020-02-15 17:56:40 +0900

    wrap up replacing to function

--- src/lambda.rs
+++ src/lambda.rs
@@ -4,18 +4,28 @@ use lambda_calculus::{parse, Classic};
 fn test() {
     let result = parse(&"λa.a", Classic);
     assert_eq!(result, parse(&"λa.a", Classic));
-    let mut ss: String =
-        "λL R Q N F. forall x.(Q(λG.N(λy.(G(y) & (x = y))), λy.True) -> F(x))".to_string();
-    let offset = ss.find('.').unwrap_or(ss.len());
-    let t: String = ss.drain(..offset).collect();
+}
+
+#[test]
+fn test_space() {
+    assert_eq!(
+        space2lam(
+            "λL R Q N F. forall x.(Q(λG.N(λy.(G(y) & (x = y))), λy.True) -> F(x))".to_string()
+        ),
+        "λL.λR.λQ.λN.λF. forall x.(Q(λG.N(λy.(G(y) & (x = y))), λy.True) -> F(x))".to_string()
+    );
+}
+
+fn space2lam(mut input: String) -> String {
+    let offset = input.find('.').unwrap_or(input.len());
+    let t: String = input.drain(..offset).collect();
     let tt = t.replace(" ", ".λ");
-    let rrr = tt + &ss;
-    let s = "λL.λR.λQ.λN.λF. forall x.(Q(λG.N(λy.(G(y) & (x = y))), λy.True) -> F(x))".to_string();
-    assert_eq!(rrr, s);
+    tt + &input
 }
 
 pub fn string2lambda() {
-    let s = &"λL.λR.λQ.λN.λF. forall x.(Q(λG.N(λy.(G(y) & (x = y))), λy.True) -> F(x))";
-    let p = parse(s, Classic);
+    let s = "λL.λR.λQ.λN.λF. forall x.(Q(λG.N(λy.(G(y) & (x = y))), λy.True) -> F(x))".to_string();
+    let replaced = space2lam(s);
+    let p = parse(&replaced, Classic);
     println!("{:#?}", p);
 }
