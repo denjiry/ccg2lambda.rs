182591a7938d1d9f07264ca4b3e8fa38d197ee37
kento tajiri <k.denjiry@gmail.com>
2020-04-26 02:21:24 +0900

    add minimul error case

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,8 @@ use ccg2lambda_rs::lambda::cmb;
 use ccg2lambda_rs::parse::parse_jigg;
 
 fn main() {
-    let mut input = "\\G F.F& -(G(A&B) -> -C)";
+    let mut input = "\\G F.G( G(A&B) -> C)";
+    let mut input = "G(F(G))";
     println!("{:?}", cmb(&mut input));
     parse_jigg();
 }
